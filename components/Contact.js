ba3b05b37b81d2ec79a6b35418365dbea66f8c56
CodingForYah <codingforyah@gmail.com>
2021-11-20 23:31:42 -0500

    c

--- components/Contact.js
+++ components/Contact.js
@@ -19,7 +19,6 @@ function Contact() {
                 <div className="flex lg:p-0 justify-center p-4 space-y-4 flex-col text-left">
                     <a href="mailto:JohnJamesUX@gmail.com"><h1 className="font-bold text-gr-400 animate-bounce justify-center flex">JohnJamesUX@gmail.com</h1></a>
                     <h1 className=" justify-center flex">859-536-0453</h1>
-                    <h1 className=" justify-center flex">Contact me soon! I won't be on the market long.</h1>
                     <a className="ml-auto mr-auto w-3/4" target="_blank" href="JohnJamesUX.pdf" download="John James UX"><button className="p-2 justify-center ml-auto mr-auto flex rounded-full bg-black text-white text-sm bg-black font-semibold ">Download Resume</button></a>
                 </div>
                 {/*Buttons*/}
