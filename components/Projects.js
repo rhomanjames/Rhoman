dca087f3fdbc3ef152d1e49ee4d5ebe986ac892b
CodingForYah <codingforyah@gmail.com>
2021-11-26 08:52:54 -0500

    new

--- components/Projects.js
+++ components/Projects.js
@@ -135,22 +135,6 @@ function Projects() {
                     
                 </div>
 
-                 {/*88 Hacks*/}
-                 <div className="flex mt-8  lg:p-0 p-4 transition  space-y-4 flex-col text-center">
-                <Image 
-                    src= "/DesignHacks.png"
-                    alt="88 Design Hacks"
-                    className="rounded-full
-                    object-contain border-4 border-solid border-gray-100" 
-                    height={300}
-                    width={300}/> 
-                    <h1 id="text" className="text-lg md:text-lg lg:text-xl font-bold">88 UI/UX Design Hacks You Must Know</h1>
-                    <h4 className="md:w-3/4 ml-auto mr-auto">An eBook I'm writing to help new UI/UX developers understand core concepts. Still in production.</h4>
-                    {/*Buttons*/}
-                    <div className="flex w-full justify-center space-x-4 border-b-2 pb-8">
-                        <a target="_blank" download="88 UI and UX Design Hacks You Must Know" href="/88Hacks.pdf"><h4 id="location" className="text-xs p-2 rounded-full font-semibold bg-black text-white animate-pulse  hover:bg-white hover:text-black border-2 border-transparent hover:border-black cursor-pointer transition">Preview Book</h4></a>
-                    </div> 
-                </div>
 
                 {/*Onward*/}
                 <div className="flex mt-8 lg:p-0 transition justify-center w-full  p-4 border-b-2 object-left space-y-4 flex-col text-center ">
@@ -178,29 +162,21 @@ function Projects() {
                         
                     </div>
                 </div>
-
-                {/*Design Hacks*/}
-                <div className="flex mt-8  lg:p-0 p-4 transition border-b-2 border-gray-50  space-y-4 flex-col text-center">
+                {/*88 Hacks*/}
+                <div className="flex mt-8  lg:p-0 p-4 transition  space-y-4 flex-col text-center">
                 <Image 
-                    src= "/Scriptures.png"
-                    alt="Scripture"
+                    src= "/DesignHacks.png"
+                    alt="88 Design Hacks"
                     className="rounded-full
                     object-contain border-4 border-solid border-gray-100" 
-                    height={216}
-                    width={384}/> 
-                    <h1 id="text" className="text-lg md:text-lg lg:text-xl font-bold">Daily Scripture Generator</h1>
-                    <h4>Side project -- Random scripture generator with a clean UI that is structured around object orentied programming. Share to social media with one click.</h4>
-                    {/*Hashtags*/}
-                    <div className="flex overflow-x-scroll w-full justify-center sm:overflow-x-visible space-x-4 pb-4">
-                        <h4 id="location" className="text-xs bg-white border-2  p-2 rounded-full font-semibold text-black">React JS</h4>
-                        <h4 id="location" className="text-xs bg-white border-2  p-2 rounded-full font-semibold text-black">Next JS</h4>
-                        <h4 id="location" className="text-xs bg-white border-2  p-2 rounded-full font-semibold text-black">Tailwind CSS</h4>
-                    </div>
+                    height={300}
+                    width={300}/> 
+                    <h1 id="text" className="text-lg md:text-lg lg:text-xl font-bold">88 UI/UX Design Hacks You Must Know</h1>
+                    <h4 className="md:w-3/4 ml-auto mr-auto">An eBook I'm writing to help new UI/UX developers understand core concepts. Still in production.</h4>
                     {/*Buttons*/}
-                    <div className="flex w-full justify-center space-x-4 pb-8">
-                    <a target="_blank"  href="https://github.com/rhomanjames/daily-scripture-generator"><h4 id="location" className="text-xs bg-white p-2 rounded-full font-semibold hover:bg-white hover:text-black border-2 border-transparent hover:border-black transition cursor-pointer text-black">GitHub</h4></a>
-                        <a target="_blank"  href="https://yahdaily.vercel.app/"> <h4 id="location" className="text-xs  animate-pulse bg-black   p-2 rounded-full font-semibold hover:bg-white hover:text-black border-2 border-transparent hover:border-black cursor-pointer transition text-white">Live Demo</h4></a> 
-                    </div>
+                    <div className="flex w-full justify-center space-x-4 border-b-2 pb-8">
+                        <a target="_blank" download="88 UI and UX Design Hacks You Must Know" href="/88Hacks.pdf"><h4 id="location" className="text-xs p-2 rounded-full font-semibold bg-black text-white animate-pulse  hover:bg-white hover:text-black border-2 border-transparent hover:border-black cursor-pointer transition">Preview Book</h4></a>
+                    </div> 
                 </div>
                 
             </div>
